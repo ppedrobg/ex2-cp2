8cd42a92649675f2c056a89645d5c80726e11539
ppedrobg <m551061@fiap.com.br>
2023-09-05 20:55:35 -0300

    adicionando a main

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,30 @@
+import React from 'react'
+import { ReactDOM } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
 import EditarProduto from './routes/EditarProdutos/index.jsx'
 
-const router = createBrowserRouter([
-  {
-    path: "/"
-    element: <App/>,
-    errorElement: <Error/>,
-    children:[
-      {
-        path: "/"
-        element:<Home />
-      },
-      {
-        path: "/prdutos"
-        element:<Produtos/>
-      },
-      {
-        path:"/editar/:id",
-        element:<EditarProduto/>
-      }
 
-    ]
-  }
+
+const router = createBrowserRouter([
+  {path: "/" , element: <App/>, errorElement: <Error/>,
+    children:[
+      {path: "/" , element: <Home/>},
+      {path: "/produtos" , element: <Produtos/>},
+      {path: "/editar/produtos/:id" , element: <EditarProduto/>},
+    ]}
 ])
 
+ 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>,
+<React.StrictMode>
+<RouterProvider router={router}/>
+</React.StrictMode>,
 )
